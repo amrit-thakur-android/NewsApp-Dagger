7e9f981c263096d54e80aee0255e7d32cc5fbe39
amrit-spoton <athakur@spoton.com>
2025-08-15 11:52:37 +0530

    Remove apiKey from service interface as we will use interceptor to pass apiKey.

--- data/src/main/java/com/amritthakur/newsapp/remote/api/NewsApiService.kt
+++ data/src/main/java/com/amritthakur/newsapp/remote/api/NewsApiService.kt
@@ -4,27 +4,22 @@ import com.amritthakur.newsapp.remote.response.NewsResponse
 import com.amritthakur.newsapp.remote.response.SourcesResponse
 import retrofit2.Response
 import retrofit2.http.GET
-import retrofit2.http.Header
 import retrofit2.http.Query
 
 interface NewsApiService {
 
     @GET("/v2/top-headlines")
     suspend fun getTopHeadlines(
-        @Header("X-Api-Key") apiKey: String,
         @Query("sources") source: String? = null,
         @Query("country") country: String? = null,
         @Query("language") language: String? = null
     ): Response<NewsResponse>
 
     @GET("/v2/top-headlines/sources")
-    suspend fun getSources(
-        @Header("X-Api-Key") apiKey: String,
-    ): Response<SourcesResponse>
+    suspend fun getSources(): Response<SourcesResponse>
 
     @GET("/v2/everything")
     suspend fun searchNews(
-        @Header("X-Api-Key") apiKey: String,
         @Query("q") query: String
     ): Response<NewsResponse>
 }
