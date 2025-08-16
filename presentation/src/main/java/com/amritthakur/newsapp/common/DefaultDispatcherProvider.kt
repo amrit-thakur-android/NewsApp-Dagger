36446b83b44f19294f60297fda6d69d5f688caed
amrit-spoton <athakur@spoton.com>
2025-08-16 11:43:32 +0530

    Add dispatcher provider pattern.

---
+++ presentation/src/main/java/com/amritthakur/newsapp/common/DefaultDispatcherProvider.kt
@@ -0,0 +1,15 @@
+package com.amritthakur.newsapp.common
+
+import kotlinx.coroutines.CoroutineDispatcher
+import kotlinx.coroutines.Dispatchers
+
+class DefaultDispatcherProvider : DispatcherProvider {
+
+    override val main: CoroutineDispatcher = Dispatchers.Main
+
+    override val io: CoroutineDispatcher = Dispatchers.IO
+
+    override val default: CoroutineDispatcher = Dispatchers.Default
+
+    override val unconfined: CoroutineDispatcher = Dispatchers.Unconfined
+}
---
+++ presentation/src/main/java/com/amritthakur/newsapp/common/TestDispatcherProvider.kt
@@ -0,0 +1,15 @@
+package com.amritthakur.newsapp.common
+
+import kotlinx.coroutines.CoroutineDispatcher
+import kotlinx.coroutines.Dispatchers
+
+class TestDispatcherProvider : DispatcherProvider {
+
+    override val main: CoroutineDispatcher = Dispatchers.Unconfined
+
+    override val io: CoroutineDispatcher = Dispatchers.Unconfined
+
+    override val default: CoroutineDispatcher = Dispatchers.Unconfined
+
+    override val unconfined: CoroutineDispatcher = Dispatchers.Unconfined
+}
