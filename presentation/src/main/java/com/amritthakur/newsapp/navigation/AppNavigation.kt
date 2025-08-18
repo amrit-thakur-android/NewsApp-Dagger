d87d069d9afb98174a82309bb80fe02723d70cde
amrit-spoton <athakur@spoton.com>
2025-08-18 16:45:04 +0530

    Add app navigation nav host.

---
+++ presentation/src/main/java/com/amritthakur/newsapp/navigation/AppNavigation.kt
@@ -0,0 +1,54 @@
+package com.amritthakur.newsapp.navigation
+
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.remember
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+
+@Composable
+fun AppNavigation() {
+    val navController = rememberNavController()
+    val navigationCoordinator = remember { NavigationCoordinator(navController) }
+
+    val navigationEvent by NavigationChannel.navigationEvent
+        .collectAsStateWithLifecycle()
+
+    LaunchedEffect(navigationEvent) {
+        navigationEvent?.let { event ->
+            navigationCoordinator.handleNavigationEvent(event)
+        }
+    }
+
+    NavHost(
+        navController = navController,
+        startDestination = Screen.Home.route
+    ) {
+        composable(Screen.Home.route) {
+
+        }
+
+        composable(Screen.News.route) {
+
+        }
+
+        composable(Screen.Sources.route) {
+
+        }
+
+        composable(Screen.Countries.route) {
+
+        }
+
+        composable(Screen.Languages.route) {
+
+        }
+
+        composable(Screen.Search.route) {
+
+        }
+    }
+}
