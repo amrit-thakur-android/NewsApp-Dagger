06e32c8f44f0540fdbfd2d9ca3f1025e28abf170
amrit-spoton <athakur@spoton.com>
2025-08-19 18:30:08 +0530

    Add application component.

---
+++ app/src/main/java/com/amritthakur/newsapp/di/ApplicationComponent.kt
@@ -0,0 +1,37 @@
+package com.amritthakur.newsapp.di
+
+import android.app.Application
+import com.amritthakur.newsapp.NewsApplication
+import com.amritthakur.newsapp.navigation.NavigationChannel
+import com.amritthakur.newsapp.viewmodel.HomeViewModel
+import com.amritthakur.newsapp.viewmodel.NewsViewModel
+import dagger.BindsInstance
+import dagger.Component
+import javax.inject.Singleton
+
+@Singleton
+@Component(
+    modules = [
+        ApplicationModule::class,
+        NetworkModule::class,
+        RepositoryModule::class,
+        DispatcherModule::class,
+    ]
+)
+interface ApplicationComponent {
+
+    fun inject(application: NewsApplication)
+
+    // Expose functions
+    fun homeViewModel(): HomeViewModel
+    fun newsViewModel(): NewsViewModel
+    fun navigationChannel(): NavigationChannel
+
+    @Component.Builder
+    interface Builder {
+        @BindsInstance
+        fun application(application: Application): Builder
+
+        fun build(): ApplicationComponent
+    }
+}
