a8370f54a787e9425d2bb2ca629c4c31b4923b0b
amrit-spoton <athakur@spoton.com>
2025-08-19 18:29:21 +0530

    Remove unused import.

--- app/src/main/java/com/amritthakur/newsapp/MainActivity.kt
+++ app/src/main/java/com/amritthakur/newsapp/MainActivity.kt
@@ -8,7 +8,6 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.ui.Modifier
-import com.amritthakur.newsapp.navigation.AppNavigation
 import com.amritthakur.newsapp.ui.theme.NewsAppDaggerTheme
 
 class MainActivity : ComponentActivity() {
