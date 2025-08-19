5b1a46c64df162778be5957c6800380bb6713c15
amrit-spoton <athakur@spoton.com>
2025-08-19 18:39:45 +0530

    Fix import.

--- app/src/main/java/com/amritthakur/newsapp/MainActivity.kt
+++ app/src/main/java/com/amritthakur/newsapp/MainActivity.kt
@@ -8,6 +8,7 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.ui.Modifier
+import com.amritthakur.newsapp.navigation.AppNavigation
 import com.amritthakur.newsapp.ui.theme.NewsAppDaggerTheme
 
 class MainActivity : ComponentActivity() {
